3e389259c266dd91a4a22fc7f116c769f9c48d4a
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-04-03 18:54:43 +0500

    code updated

--- src/components/user/dashboard/Dashboard.jsx
+++ src/components/user/dashboard/Dashboard.jsx
@@ -1,14 +1,17 @@
 "use client";
 import dynamic from "next/dynamic";
+import Alerts from "./Alerts";
 const Map = dynamic(() => import("./Map"), { ssr: false });
 
 const Dashboard = () => {
   return (
     <section className=" grid grid-cols-1 xl:grid-cols-12  gap-4 ">
-      <div className=" col-span-12 xl:col-span-8">
+      <div className="xl:col-span-8">
         <Map />
       </div>
-      <div className=" col-span-12 xl:col-span-4">{/* <Alerts /> */}</div>
+      <div className="xl:col-span-4">
+        <Alerts />
+      </div>
     </section>
   );
 };
