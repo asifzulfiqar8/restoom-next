4505005d5c41713a157044fbe2eb413a5d1d87d2
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-04-22 18:18:59 +0500

    feat: sensor detail ui updated and integrated

--- src/components/user/dashboard/Dashboard.jsx
+++ src/components/user/dashboard/Dashboard.jsx
@@ -64,7 +64,9 @@ const Dashboard = () => {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
             {buildingData.slice(0, 3).map((building, i) => (
-              <BuildingCard data={building} key={i} />
+              <Link href={`/buildings/${building?.id}`} key={i}>
+                <BuildingCard data={building} />
+              </Link>
             ))}
           </div>
         </div>
