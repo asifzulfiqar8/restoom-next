745ae7a5b03cd8004abf71b4e017a6c4fc43254f
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-04-23 18:50:28 +0500

    feat: building stepper info integrated with redux

--- src/components/global/small/Input.jsx
+++ src/components/global/small/Input.jsx
@@ -5,7 +5,7 @@ const Input = ({ label, type = "text", shadow = false, ...rest }) => {
       <input
         {...rest}
         type={type}
-        className={`mt-2 outline-none px-4 h-[50px] border-[0.5px] border-[#66666659] rounded-xl w-full text-sm lg:text-base text-[#666666] ${
+        className={`mt-2 outline-none px-4 h-[50px] border-[0.5px] border-[#66666659] rounded-xl w-full text-sm lg:text-base text-[#3a3a3a] ${
           shadow && "shadow-input"
         }`}
       />
--- src/components/user/addBuilding/BuildingModel.jsx
+++ src/components/user/addBuilding/BuildingModel.jsx
@@ -1,12 +1,26 @@
 import Button from "@/components/global/small/Button";
+import MarkBuildingModel from "./MarkBuildingModel";
+import { useState } from "react";
 
 const BuildingModel = ({ setCurrentStep }) => {
+  const [buildingModelImage, setBuildingModelImage] = useState(null);
+  const [polygons, setPolygons] = useState([]);
+  const [file, setFile] = useState(null);
   const nextBtnHandler = () => {
     setCurrentStep((prevStep) => prevStep + 1);
   };
   return (
     <div>
       <h6 className="text-base text-primary font-medium">Building Model</h6>
+      <div className="py-10 grid place-items-center">
+        <MarkBuildingModel
+          setFile={setFile}
+          buildingModelImage={buildingModelImage}
+          setBuildingModelImage={setBuildingModelImage}
+          polygons={polygons}
+          setPolygons={setPolygons}
+        />
+      </div>
       <div className="flex items-center justify-end gap-4">
         <Button
           text="Back"
